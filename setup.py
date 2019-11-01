e5e3bcb881b99942b6aab4d57c2ea814e861d2fa
Tanner Burns <tannerburns@Tanners-MacBook-Pro.local>
2019-10-31 20:39:46 -0500

    new requirement -> vast library for multitasking; remove dist, build, and egg from wheel install since no longer needed

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name= 'halo',
-    version= '0.34.7',
+    version= '0.45.8',
     packages= find_packages(),
     include_package_data= True,
     description= 'Utility library for parsing, analysis, clustering, and classifying.',
@@ -18,9 +18,10 @@ setup(
     author= 'Tanner Burns',
     author_email= 'tjburns102@gmail.com',
     install_requires=[
-        "pandas",
-        "pandas_profiling",
-        "scikit-learn"
+        'pandas',
+        'pandas_profiling',
+        'scikit-learn',
+        'vast @ https://github.com/TannerBurns/vast/archive/master.zip'
     ],
     classifiers=[
         'Operating System :: OS Independent',
